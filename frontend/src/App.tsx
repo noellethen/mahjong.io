c82d929ddbabe9d7aaa3ffa92cfdc7065a345eda
Ryan Ng <flashsstudioz@gmail.com>
2025-05-19 17:21:11 +0800

    PUSH PLEASE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [array, setArray] = useState([]);
 
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:5000/api/users");
+    console.log(response.data.users);
+    setArray(response.data.users);
+  }
+
+  useEffect(() => {
+    fetchAPI()
+  },[])
   return (
     <>
       <div>
@@ -21,9 +32,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          {array.map((user, index) => (
+            <div key={index}>
+              <span>{user}</span>
+              <br></br>
+            </div>
+          ))}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
