d16f91cd2171d042c0bb9f09bf5de32625a1a3c3
Ryan Ng <flashsstudioz@gmail.com>
2025-05-20 17:09:48 +0800

    Configured the Backend now to handle the routing (Integration yipeee)

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,20 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
 
-@app.route("/api/users", methods=['GET'])
-def users():
-    return jsonify(
-        {
-            "users": [
-                'alice',
-                'bob',
-                'charlie'
-            ]
-        }
-    )
+@app.route("/")
+def serve_login():
+    return send_from_directory(app.template_folder, 'index.html')
+
+@app.route('/gamemode')
+def serve_gamemode():
+    return send_from_directory(app.template_folder, 'gamemode.html')
+
+@app.route('/quizmode')
+def serve_quizmode():
+    return send_from_directory(app.template_folder, 'quizmode.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
