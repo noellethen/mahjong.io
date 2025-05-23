409229a27eaa4f5b99d6c2a80e4d662e159fd309
noellethen <nehtelleon@gmail.com>
2025-05-23 18:04:08 +0800

    created base db stuff (idk if its correct)

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,8 +1,25 @@
 from flask import Flask, send_from_directory, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
 
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]
 cors = CORS(app, origins='*')
+db.init_app(app)
+
+class Base(DeclarativeBase):
+    pass
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key = True)
+    username: Mapped[str] = mapped_column(unique = True)
+    email: Mapped[str]
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def serve_login():
