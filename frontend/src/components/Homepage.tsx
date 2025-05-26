12a0b16e0e3c18e6e6a520aca987c7ffe2c47cd6
noellethen <nehtelleon@gmail.com>
2025-05-26 20:54:55 +0800

    sign in done

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -1,34 +1,59 @@
+import React from "react";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 function Homepage() {
   const navigate = useNavigate();
+  const { session, signOut } = UserAuth();
+
+  const handleSignOut = async (e) => {
+    e.preventDefault();
+    try {
+      await signOut();
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="flex flex-col space-y-4 w-40">
-        <button
-          onClick={() => navigate("/gamemode")}
-          className="w-full rounded-md border px-4 py-2"
-        >
-          Play
-        </button>
-
-        <button
-          onClick={() => navigate("/shop")}
-          className="w-full rounded-md border px-4 py-2"
-        >
-          Shop
-        </button>
-
-        <button
-          onClick={() => navigate("/customise")}
-          className="w-full rounded-md border px-4 py-2"
-        >
-          Customise
-        </button>
+    <>
+      <div>
+        <h2>Welcome, {session?.email}</h2>
       </div>
-    </div>
+      <div className="h-screen flex items-center justify-center">
+        <div className="flex flex-col space-y-4 w-40">
+          <button
+            onClick={() => navigate("/gamemode")}
+            className="w-full rounded-md border px-4 py-2"
+          >
+            Play
+          </button>
+
+          <button
+            onClick={() => navigate("/shop")}
+            className="w-full rounded-md border px-4 py-2"
+          >
+            Shop
+          </button>
+
+          <button
+            onClick={() => navigate("/customise")}
+            className="w-full rounded-md border px-4 py-2"
+          >
+            Customise
+          </button>
+        </div>
+      </div>
+      <div>
+        <p
+          onClick={handleSignOut}
+          className="hover:cursor-pointer border inline-block px-4 py-3 mt-4"
+        >
+          Sign Out
+        </p>
+      </div>
+    </>
   );
 }
 
--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { session, signInUser } = UserAuth();
+  const navigate = useNavigate();
+  console.log(session);
+
+  const handleSignin = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+    try {
+      const result = await signInUser(email, password);
+
+      if (!result.success) {
+        setError(result.error.message || "Signin failed");
+        return;
+      }
+
+      navigate("/homepage");
+    } catch (error) {
+      setError("An error occurred.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSignin} className="max-w-md m-auto pt-24">
+        <h2 className="font-bold pb-2">Sign in</h2>
+        <div className="flex flex-col py-4">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="p-3 mt-6 bg-gray-950"
+            type="email"
+            name=""
+            id=""
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="p-3 mt-6 bg-gray-950"
+            type="password"
+            name=""
+            id=""
+          />
+          <button type="submit" disabled={loading} className="mt-6 w-full">
+            Sign In
+          </button>
+          {error && <p className="text-red-600 text-center pt-4">{error}</p>}
+        </div>
+        <p>
+          Don't have an account? <Link to="/signup">Sign up!</Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Signin;
